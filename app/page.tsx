cdd3c18dfe0b4f425765e27d1030b25314a441f0
Alicia Park <aliciayerin@gmail.com>
2024-02-26 18:26:45 -0600

    auto-commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
 import Image from "next/image";
+import UsersPage from "./users/page";
 
 export default function Home() {
   return (
-    <main>
-      <div style={{width: '100%', height: '100%', justifyContent: 'flex-start', alignItems: 'center', display: 'inline-flex'}}>
-    <img style={{width: 65, height: 64}} src="https://via.placeholder.com/65x64" />
-    <div style={{color: 'black', fontSize: 32, fontFamily: 'Jura', fontWeight: '400', wordWrap: 'break-word'}}>UTalk</div>
-</div>
-
-<div style={{color: 'black', fontSize: 32, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>log in</div>
-    </main>
+    <UsersPage></UsersPage>
   );
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,9 +2,59 @@ import React from 'react'
 
 const UsersPage = () => {
   return (
-    <div>UsersPage
-        <p>dwajdlwadl</p>
+    <div style={{width: '100%', height: '100%', padding: 16, background: '#ECECEC', borderRadius: 16, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 18, display: 'inline-flex'}}>
+    <div style={{width: 633, justifyContent: 'space-between', alignItems: 'center', display: 'inline-flex'}}>
+        <div style={{justifyContent: 'flex-start', alignItems: 'center', gap: 8, display: 'flex'}}>
+            <div style={{width: 48, height: 48, background: 'white', borderRadius: 13}} />
+            <div style={{flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 4, display: 'inline-flex'}}>
+                <div style={{width: 139, height: 26, color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Anonymous<br/><br/></div>
+                <div style={{width: 96, height: 11, color: 'black', fontSize: 10, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>XX / XX   XX : XX</div>
+            </div>
+        </div>
+        <div style={{justifyContent: 'flex-start', alignItems: 'flex-start', gap: 7, display: 'flex'}}>
+            <div style={{width: 6, height: 6, background: 'white', borderRadius: 9999}} />
+            <div style={{width: 6, height: 6, background: 'white', borderRadius: 9999}} />
+            <div style={{width: 6, height: 6, background: 'white', borderRadius: 9999}} />
+        </div>
     </div>
+    <div style={{flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 14, display: 'flex'}}>
+        <div style={{width: 233, height: 36, color: 'black', fontSize: 32, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Header / Title</div>
+        <div style={{width: 648, color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODY BODd<br/><br/><br/><br/></div>
+    </div>
+    <div style={{justifyContent: 'flex-start', alignItems: 'flex-end', gap: 21, display: 'inline-flex'}}>
+        <div style={{width: 23, height: 23, position: 'relative', mixBlendMode: 'multiply'}}>
+            <img style={{width: 23, height: 23, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/23x23" />
+            <div style={{width: 23, height: 23, left: 0, top: -1, position: 'absolute', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'inline-flex'}}>
+                <div style={{width: 23, height: 23, background: '#BF5700'}} />
+            </div>
+        </div>
+        <div style={{width: 23, height: 23, position: 'relative'}}>
+            <img style={{width: 23, height: 23, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/23x23" />
+            <div style={{width: 23, height: 23, left: 0, top: 0, position: 'absolute', mixBlendMode: 'multiply'}}>
+                <img style={{width: 23, height: 23, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/23x23" />
+                <div style={{width: 23, height: 23, left: 0, top: 0, position: 'absolute', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'inline-flex'}}>
+                    <div style={{width: 23, height: 23, background: 'white'}} />
+                </div>
+            </div>
+            <img style={{width: 15.39, height: 15.39, left: 3.81, top: 2.66, position: 'absolute'}} src="https://via.placeholder.com/15x15" />
+            <div style={{width: 15.39, height: 15.39, left: 3.81, top: 2.66, position: 'absolute', mixBlendMode: 'multiply'}}>
+                <img style={{width: 15.39, height: 15.39, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/15x15" />
+                <div style={{width: 23, height: 23, left: -4.83, top: -8.12, position: 'absolute', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'inline-flex'}}>
+                    <div style={{width: 23, height: 23, background: '#BF5700'}} />
+                </div>
+            </div>
+        </div>
+        <div style={{width: 23, height: 23, position: 'relative'}}>
+            <img style={{width: 23, height: 23, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/23x23" />
+            <div style={{width: 23, height: 23, left: 0, top: 0, position: 'absolute', mixBlendMode: 'multiply'}}>
+                <img style={{width: 23, height: 23, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/23x23" />
+                <div style={{width: 23, height: 23, left: 0, top: 0, position: 'absolute', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'inline-flex'}}>
+                    <div style={{width: 23, height: 23, background: '#E7C309'}} />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
   )
 }
